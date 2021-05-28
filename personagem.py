6ad5529102cf13e15a87529aa3b8d1545564f576
hyagobsantos <hyagobezerra64@gmail.com>
2021-05-28 00:05:11 -0300

    Atualização Projeto 0.123

--- personagem.py
+++ personagem.py
@@ -1,49 +1,15 @@
 class Personagem:
-    def __init__(self, nome = '',sexo = '', idade = 0, ocupacao = 0, saldoBancario = 0):
-        self.nome = nome
-        self.sexo = sexo
-        self.idade = idade
-        self.ocupacao = ocupacao
-        self.saldo = saldoBancario
+    def __init__(self):
+        self.sujo = True
+        self.fome = True
+        self.medicado = False
+        self.dinheiro = 0
+        self.salario = 100
     
+    def __str__(self): # Pesquisar significado dessa função especifica.
+        return "Você está " + ("sujo" if self.sujo else "limpo")+", "+("com" if self.fome else "sem")+" fome e "+("" if self.medicado else "não ")+"tomou sua medicação. Você tem "+str(self.dinheiro)+" reais na sua conta."
 
-    def validaSaldo(self, valorAtual):
-        while True:
-            valorAtual = float(input("Você Digitou o Valor 0, Digite Novamente:"))
-            if valorAtual > 0: 
-                self.saldo = valorAtual
-                break
-                
-    def localAtual(self, camihada):
-        local = camihada
-        print(f"Local Atual: {local}")
-
-    
-    def alimentar(self, comida):
-        escolha = ""
-        quantidade = 0
-        restante = 8
-        # print('Estou com fome') 
-        if comida == 1:
-            print('Hummm! Você comeu chocolate') 
-        elif comida == 2:
-            print('o hamburger estava uma delícia') 
-        elif comida == 3: 
-            escolha = str(input("Escolha o Sabor da Pizza: "))
-            print(f"Você Escolheu o Sabor {escolha} é Comeu um Pedaço")
-            while True:
-                quantidade = str(input('deseja comer mais um pedaço [S] - [N]').strip().upper()[0])
-                if quantidade in 'S':
-                    restante -= 1
-                    print(f"Parece que está muito bom vc comeu +1 Pedaço e tem mais {restante}")
-                    if restante == 0:
-                        print("Você Já Comeu todos os 8 Pegaços")
-                        break
-                else:
-                    break
-
-    def mostarObjeto(self):
-        print(f"\nNome: {self.nome}\nGenero: {self.sexo}\nIdade: {self.idade}\nOcupação: {self.ocupacao}\nSaldo: {self.saldo}")
-
-
-
+    def dormir(self):
+        self.sujo = True
+        self.fome = True
+        self.medicado = False
