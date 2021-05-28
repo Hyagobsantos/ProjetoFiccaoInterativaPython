980bf10bfb772a16a7574ad79d8e1a1ae0ee365d
hyagobsantos <hyagobezerra64@gmail.com>
2021-05-28 00:29:24 -0300

    Atualizaçao projeto

---
+++ horario.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+class HoraAtual:
+    def __init__(self, horaAtual, minutoAtual, diaAtual):
+        self.dia = diaAtual
+        self.min = minutoAtual
+        self.hora = horaAtual
+        
+
+    def avancaTempo(): 
+
+
+
+
+
