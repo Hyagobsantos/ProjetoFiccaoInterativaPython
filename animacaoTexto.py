b7fcf5f0af81fd8132c32324ed26ff1b9a1314b2
hyagobsantos <hyagobezerra64@gmail.com>
2021-05-29 09:13:01 -0300

    Atualização Projeto 0.0001

---
+++ animacaoTexto.py
@@ -0,0 +1,12 @@
+import time, sys
+
+def animacao(texto):
+    frase = texto
+
+    for i in list(frase):
+        print(i, end='')
+        #O stdout só é atualizado quando há nova linha e como nós estamos mandando tudo na mesma é preciso forçar a atualização.
+        #@josucka vc ta falando de qual parte aqui nesse comentario... eu meio que nao entendi pq vc ja fez esse import
+        sys.stdout.flush()
+        time.sleep(0.01)
+ 
